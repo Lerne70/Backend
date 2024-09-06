248bc4cbb68c29b7126717b0547c6e947864e52c
Luis Aranda <56684878+Lerne70@users.noreply.github.com>
2024-09-06 00:01:17 -0600

    add orm
    Se agrego el orm de entity framework

---
+++ Backend/Controllers/BeerController.cs
@@ -0,0 +1,50 @@
+﻿using Backend.DTOs;
+using Backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BeerController : ControllerBase
+    {
+        private StoreContext _context;
+
+        public BeerController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<BeerDto>> Get() => 
+            await _context.Beers.Select(b => new BeerDto
+            {
+                Id = b.BeerID,
+                Name = b.Name,
+                BrandID = b.BrandID,
+            }).ToListAsync();
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BeerDto>> GetById(int id)
+        {
+            var beer = await _context.Beers.FindAsync(id);
+
+            if (beer == null)
+            {
+                return NotFound();
+            }
+
+            var beerDto = new BeerDto
+            {
+                Id = beer.BeerID,
+                Name = beer.Name,
+                BrandID = beer.BrandID,
+                Alcohol = beer.Alcohol,
+            };
+
+            return Ok(beerDto);
+        }
+    }
+}
